bcdafb55750117bc9293c580854fffd34e16f8ea
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-04-01 15:12:55 -0700

    improved styling, adding content

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,92 +19,102 @@ function Home(props) {
           <div className="doubleContainer">
             <div className="singleContainer">
               <img className="entryImg" src={joshImg} alt="Image Of Josh" />
-              <p>
-                Hi, my name is Josh Luscombe, and I'm a full-stack web developer
-                currently based in Fresno, California. I host my portfolio here
-                where I will provide frequent updates on my latest projects and
-                career status! (You can check out my portfolio details{" "}
-                <Link to="/work">here</Link>)
-              </p>
+              <div className="descriptionContainer">
+                <p>
+                  Hi, my name is Josh Luscombe, and I'm a full-stack web
+                  developer currently based in Fresno, California. I host my
+                  portfolio here where I will provide frequent updates on my
+                  latest projects and career status! (You can check out my
+                  portfolio details <Link to="/work">here</Link>)
+                </p>
+              </div>
             </div>
             <div className="singleContainer">
               <img className="entryImg" src={rocksImg} alt="Image Of Rocks" />
-              <p>
-                My background with computer science originated in high school. I
-                took several courses in HTML/CSS, Java, and game development.
-                After 6 years of studying and working in healthcare, I can't
-                seem to want to do anything else other than code! I am very
-                grateful to Lambda that I can attend their course and gain
-                valuable skills.
-              </p>
+              <div className="descriptionContainer">
+                <p>
+                  My background with computer science originated in high school.
+                  I took several courses in HTML/CSS, Java, and game
+                  development. After 6 years of studying and working in
+                  healthcare, I can't seem to want to do anything else other
+                  than code! I am very grateful to Lambda that I can attend
+                  their course and gain valuable skills.
+                </p>
+              </div>
             </div>
           </div>
         </div>
         <div className="entryItem">
           <h3>I like making thing</h3>
-          <p>
-            Cow pepper jack danish fontina. Swiss cheesecake edam emmental
-            cheese on toast bocconcini everyone loves lancashire. Hard cheese
-            port-salut taleggio parmesan jarlsberg fromage frais melted cheese
-            halloumi. Hard cheese cheese on toast edam rubber cheese gouda
-            mascarpone when the cheese comes out everybody's happy red
-            leicester. Squirty cheese port-salut caerphilly cheesecake manchego
-            mascarpone danish fontina smelly cheese. Mascarpone cheesecake
-            dolcelatte cheesy feet stilton queso pecorino cheese slices.
-            Emmental melted cheese cheesy feet who moved my cheese babybel
-            cauliflower cheese cheddar stilton. Parmesan jarlsberg paneer cheese
-            strings cheese and biscuits camembert de normandie.
-          </p>
+          <div className="descriptionContainer">
+            <p>
+              Cow pepper jack danish fontina. Swiss cheesecake edam emmental
+              cheese on toast bocconcini everyone loves lancashire. Hard cheese
+              port-salut taleggio parmesan jarlsberg fromage frais melted cheese
+              halloumi. Hard cheese cheese on toast edam rubber cheese gouda
+              mascarpone when the cheese comes out everybody's happy red
+              leicester. Squirty cheese port-salut caerphilly cheesecake
+              manchego mascarpone danish fontina smelly cheese. Mascarpone
+              cheesecake dolcelatte cheesy feet stilton queso pecorino cheese
+              slices. Emmental melted cheese cheesy feet who moved my cheese
+              babybel cauliflower cheese cheddar stilton. Parmesan jarlsberg
+              paneer cheese strings cheese and biscuits camembert de normandie.
+            </p>
+          </div>
         </div>
 
         <div className="entryItem">
           <h3>SITES 4 TH WRLD</h3>
-          <p>
-            Brie the big cheese roquefort. Chalk and cheese who moved my cheese
-            babybel roquefort melted cheese cheese and wine stinking bishop
-            mozzarella. Jarlsberg pepper jack bocconcini parmesan swiss cheese
-            slices dolcelatte stinking bishop. Brie cheesy feet chalk and cheese
-            camembert de normandie cheesy grin stinking bishop red leicester
-            edam. Cheeseburger lancashire caerphilly feta macaroni cheese
-            stinking bishop swiss cream cheese. When the cheese comes out
-            everybody's happy melted cheese cheese triangles the big cheese hard
-            cheese cheesy grin brie lancashire. Stilton say cheese when the
-            cheese comes out everybody's happy cauliflower cheese swiss blue
-            castello red leicester taleggio. Brie mascarpone.
-          </p>
+          <div className="descriptionContainer">
+            <p>
+              Brie the big cheese roquefort. Chalk and cheese who moved my
+              cheese babybel roquefort melted cheese cheese and wine stinking
+              bishop mozzarella. Jarlsberg pepper jack bocconcini parmesan swiss
+              cheese slices dolcelatte stinking bishop. Brie cheesy feet chalk
+              and cheese camembert de normandie cheesy grin stinking bishop red
+              leicester edam. Cheeseburger lancashire caerphilly feta macaroni
+              cheese stinking bishop swiss cream cheese. When the cheese comes
+              out everybody's happy melted cheese cheese triangles the big
+              cheese hard cheese cheesy grin brie lancashire. Stilton say cheese
+              when the cheese comes out everybody's happy cauliflower cheese
+              swiss blue castello red leicester taleggio. Brie mascarpone.
+            </p>
+          </div>
         </div>
 
         <div className="entryItem">
           <h3>Code for a greater good</h3>
-
-          <p>
-            Cut the cheese gouda squirty cheese. Everyone loves fondue
-            port-salut the big cheese dolcelatte stilton red leicester fromage
-            frais. Port-salut blue castello roquefort pecorino mozzarella danish
-            fontina melted cheese ricotta. Mascarpone st. agur blue cheese goat
-            cheddar everyone loves cheese and biscuits the big cheese cheese on
-            toast. Gouda taleggio cheesy feet manchego squirty cheese queso
-            rubber cheese taleggio. St. agur blue cheese stinking bishop cream
-            cheese stilton cheese triangles rubber cheese taleggio monterey
-            jack. Jarlsberg fromage the big cheese.
-          </p>
+          <div className="descriptionContainer">
+            <p>
+              Cut the cheese gouda squirty cheese. Everyone loves fondue
+              port-salut the big cheese dolcelatte stilton red leicester fromage
+              frais. Port-salut blue castello roquefort pecorino mozzarella
+              danish fontina melted cheese ricotta. Mascarpone st. agur blue
+              cheese goat cheddar everyone loves cheese and biscuits the big
+              cheese cheese on toast. Gouda taleggio cheesy feet manchego
+              squirty cheese queso rubber cheese taleggio. St. agur blue cheese
+              stinking bishop cream cheese stilton cheese triangles rubber
+              cheese taleggio monterey jack. Jarlsberg fromage the big cheese.
+            </p>
+          </div>
         </div>
 
         <div className="entryItem">
           <h3>Who writes this much about cheese?</h3>
-
-          <p>
-            Paneer goat cheeseburger. Red leicester emmental mozzarella when the
-            cheese comes out everybody's happy say cheese caerphilly taleggio
-            ricotta. Emmental red leicester fromage hard cheese feta danish
-            fontina jarlsberg cream cheese. Cheesy grin cut the cheese everyone
-            loves blue castello cheesy feet rubber cheese squirty cheese cream
-            cheese. Stinking bishop the big cheese who moved my cheese cheese
-            triangles mascarpone red leicester paneer cheese strings. Brie
-            airedale smelly cheese bocconcini cow st. agur blue cheese roquefort
-            roquefort. Manchego bavarian bergkase mozzarella cow cheesecake
-            cheese triangles stilton.
-          </p>
+          <div className="descriptionContainer">
+            <p>
+              Paneer goat cheeseburger. Red leicester emmental mozzarella when
+              the cheese comes out everybody's happy say cheese caerphilly
+              taleggio ricotta. Emmental red leicester fromage hard cheese feta
+              danish fontina jarlsberg cream cheese. Cheesy grin cut the cheese
+              everyone loves blue castello cheesy feet rubber cheese squirty
+              cheese cream cheese. Stinking bishop the big cheese who moved my
+              cheese cheese triangles mascarpone red leicester paneer cheese
+              strings. Brie airedale smelly cheese bocconcini cow st. agur blue
+              cheese roquefort roquefort. Manchego bavarian bergkase mozzarella
+              cow cheesecake cheese triangles stilton.
+            </p>
+          </div>
         </div>
       </div>{" "}
     </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+//content images
+import sweetEats from "../images/sweet-eats.png";
+import userInterface from "../images/ui-1.png";
+import studybookImg from "../images/marketing-page.png";
+
 import {
   Route,
   Link,
@@ -13,37 +18,80 @@ function Work(props) {
       <div className="pageTitle">Work</div>
       <div className="contentContainer">
         <div className="entryItem">
-          <h3>I like making thing</h3>
-          <p>
-            Cow pepper jack danish fontina. Swiss cheesecake edam emmental
-            cheese on toast bocconcini everyone loves lancashire. Hard cheese
-            port-salut taleggio parmesan jarlsberg fromage frais melted cheese
-            halloumi. Hard cheese cheese on toast edam rubber cheese gouda
-            mascarpone when the cheese comes out everybody's happy red
-            leicester. Squirty cheese port-salut caerphilly cheesecake manchego
-            mascarpone danish fontina smelly cheese. Mascarpone cheesecake
-            dolcelatte cheesy feet stilton queso pecorino cheese slices.
-            Emmental melted cheese cheesy feet who moved my cheese babybel
-            cauliflower cheese cheddar stilton. Parmesan jarlsberg paneer cheese
-            strings cheese and biscuits camembert de normandie.
-          </p>
+          <h3>Product: Studybook - A Clinical Trial Recruitment App</h3>
+
+          <span class="image main">
+            <img className="entryImg" src={studybookImg} alt="" />
+          </span>
+          <div className="descriptionContainer">
+            <p>
+              In this project, our team was tasked with creating the front-end
+              design scheme based on illustrations provided by our UX design
+              team. We were able to achieve near pixel-perfect recreation, which
+              resulted in something I'm really proud of.
+            </p>
+          </div>
         </div>
 
         <div className="entryItem">
-          <h3>SITES 4 TH WRLD</h3>
-          <p>
-            Brie the big cheese roquefort. Chalk and cheese who moved my cheese
-            babybel roquefort melted cheese cheese and wine stinking bishop
-            mozzarella. Jarlsberg pepper jack bocconcini parmesan swiss cheese
-            slices dolcelatte stinking bishop. Brie cheesy feet chalk and cheese
-            camembert de normandie cheesy grin stinking bishop red leicester
-            edam. Cheeseburger lancashire caerphilly feta macaroni cheese
-            stinking bishop swiss cream cheese. When the cheese comes out
-            everybody's happy melted cheese cheese triangles the big cheese hard
-            cheese cheesy grin brie lancashire. Stilton say cheese when the
-            cheese comes out everybody's happy cauliflower cheese swiss blue
-            castello red leicester taleggio. Brie mascarpone.
-          </p>
+          <h3>User-Interface 1: Innovation On Demand</h3>
+
+          <span class="image main">
+            <img className="entryImg" src={userInterface} alt="" />
+          </span>
+          <div className="descriptionContainer">
+            <p>
+              This assignment was one of the first that incorporated hosting on
+              github. The instructions were to replicate the design file as
+              closely as possible, while utilizing the skills and best-practices
+              we had been studying.
+            </p>
+          </div>
+        </div>
+        <div className="entryItem">
+          <h3>User-Interface 2: Sweet Eats Bakery</h3>
+
+          <span class="image main">
+            <img className="entryImg" src={sweetEats} alt="" />
+          </span>
+          <div className="descriptionContainer">
+            <p>
+              While one of the earlier assignments, the Sweet Eats project was a
+              very fun introductory exercise in CSS styling. Successfully
+              drafting a website based off of a design document was very
+              rewarding!
+            </p>
+          </div>
+        </div>
+        <div className="entryItem">
+          <h3>User-Interface 2: Sweet Eats Bakery</h3>
+
+          <span class="image main">
+            <img className="entryImg" src={sweetEats} alt="" />
+          </span>
+          <div className="descriptionContainer">
+            <p>
+              While one of the earlier assignments, the Sweet Eats project was a
+              very fun introductory exercise in CSS styling. Successfully
+              drafting a website based off of a design document was very
+              rewarding!
+            </p>
+          </div>
+        </div>
+        <div className="entryItem">
+          <h3>User-Interface 2: Sweet Eats Bakery</h3>
+
+          <span class="image main">
+            <img className="entryImg" src={sweetEats} alt="" />
+          </span>
+          <div className="descriptionContainer">
+            <p>
+              While one of the earlier assignments, the Sweet Eats project was a
+              very fun introductory exercise in CSS styling. Successfully
+              drafting a website based off of a design document was very
+              rewarding!
+            </p>
+          </div>
         </div>
       </div>{" "}
     </div>
