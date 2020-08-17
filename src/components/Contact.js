996f3dcea26067da1f94dca58fd05f625bf6e805
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-08-17 12:25:35 -0700

    refactor name info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ import React from "react";
 function Contact(props) {
   return (
     <div className="pageContainer">
-      <h3 className="nameTitle">Josh Luscombe - 2020</h3>
+      <h3 className="nameTitle">Zoe Brandt - 2020</h3>
       <div className="pageTitle">Contact</div>
       <div className="contentContainer">
         <div className="entryItem">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,22 @@
 import React from "react";
-import joshImg from "../images/JoshIntro.png";
+import zoeimg from "../images/rocks.png";
 
 function Home(props) {
   return (
     <div className="pageContainer">
-      <h3 className="nameTitle">Josh Luscombe - 2020</h3>
+      <h3 className="nameTitle">Zoe Brandt - 2020</h3>
       <div className="pageTitle">Home</div>
       <div className="contentContainer">
         <div className="entryItem">
           <h3>Welcome</h3>{" "}
           <div className="doubleContainer">
             <div className="singleContainer">
-              <img className="entryImg" src={joshImg} alt="Josh" />
+              <img className="entryImg" src={zoeimg} alt="Josh" />
               <div className="descriptionContainer">
                 <p>
-                  Hi, my name is Josh Luscombe, and I'm a Full-Stack Web
-                  developer currently based in Fresno, California. You can visit
-                  my Projects page for examples of my work.
+                  Hi, my name is Zoe Brandt, and I'm a Full-Stack Web developer
+                  currently based in Fresno, California. You can visit my
+                  Projects page for examples of my work.
                 </p>
               </div>
             </div>
